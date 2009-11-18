ea835b4e3520c56c0082c4f7e93233bc2cfbb7de
Ali Kaafarani <kaafarani.ali@gmail.com>
2009-11-18 02:19:33 +0100

    Make logfile accessible from web interface

--- app/controllers/extras_controller.rb
+++ app/controllers/extras_controller.rb
@@ -1,2 +1,38 @@
 class ExtrasController < ApplicationController
+  def logs
+
+    if(params['full'])
+      maxlines = 10000
+    else
+      maxlines = 40
+    end
+    @log = ""
+
+    # first count the number of lines
+    counter = 0
+    File.open("/home/murray/gh/log-current", "r") do |log|
+      while(log.gets != nil)
+        counter = counter + 1
+      end
+    end
+
+    if(counter > maxlines)
+      skip = counter - maxlines
+    else
+      skip = 0
+    end
+
+    # now add only the specified number of lines
+    File.open("/home/murray/gh/log-current", "r") do |log|
+      while(line = log.gets)
+        if skip > 0
+          skip = skip - 1
+          next
+        end
+        @log += line
+      end
+    end
+
+    render :layout => 'empty'
+  end
 end
